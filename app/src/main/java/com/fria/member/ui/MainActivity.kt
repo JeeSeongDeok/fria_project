38e3fc08ac148b1ac38ee6542183e62d978cdd8c
JeeSeongDeok <bh939@naver.com>
2023-01-01 13:27:27 +0900

    java doc commit

--- app/src/main/java/com/fria/member/ui/MainActivity.kt
+++ app/src/main/java/com/fria/member/ui/MainActivity.kt
@@ -2,6 +2,13 @@ package com.fria.member.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fria.member.R
+
+/**
+ * MainActivity.java
+ *
+ * @author JSD
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
